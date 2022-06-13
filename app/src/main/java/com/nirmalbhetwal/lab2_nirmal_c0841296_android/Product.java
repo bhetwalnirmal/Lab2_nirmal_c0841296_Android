09e088f37b4f83631c82162e52c07c17301b5607
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-13 17:24:24 -0400

    created model class product

--- app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/Product.java
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/Product.java
@@ -1,2 +1,17 @@
-package com.nirmalbhetwal.lab2_nirmal_c0841296_android;public class Product {
+package com.nirmalbhetwal.lab2_nirmal_c0841296_android;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "product")
+public class Product {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "name")
+    private String name;
+    @ColumnInfo(name = "description")
+    private String description;
+    @ColumnInfo(name = "price")
+    private double price;
 }
