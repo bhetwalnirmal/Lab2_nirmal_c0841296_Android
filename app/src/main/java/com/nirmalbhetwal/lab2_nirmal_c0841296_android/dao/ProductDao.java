d21b0fba019c94bae93475f5d9256830dd14721c
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-13 17:29:13 -0400

    created methods on data access object class

--- app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/dao/ProductDao.java
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/dao/ProductDao.java
@@ -1,4 +1,26 @@
 package com.nirmalbhetwal.lab2_nirmal_c0841296_android.dao;
 
-public class ProductDao {
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.nirmalbhetwal.lab2_nirmal_c0841296_android.models.Product;
+
+import java.util.List;
+
+@Dao
+public interface ProductDao {
+    @Query("Select * from products");
+    List<Product> getProductList();
+
+    @Insert
+    void insertProduct(Product product);
+
+    @Update
+    void updateProduct(Product product);
+
+    @Delete
+    void deleteProduct(Product product);
 }
--- app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/models/Product.java
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/models/Product.java
@@ -4,7 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity(tableName = "product")
+@Entity(tableName = "products")
 public class Product {
     @PrimaryKey(autoGenerate = true)
     private int id;
