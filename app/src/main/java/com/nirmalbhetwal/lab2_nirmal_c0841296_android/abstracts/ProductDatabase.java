891a18d090e4b759a6619e2638394673479fb63f
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-13 18:15:04 -0400

    implemented room database

---
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/abstracts/ProductDatabase.java
@@ -0,0 +1,29 @@
+package com.nirmalbhetwal.lab2_nirmal_c0841296_android.abstracts;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.nirmalbhetwal.lab2_nirmal_c0841296_android.dao.ProductDao;
+import com.nirmalbhetwal.lab2_nirmal_c0841296_android.models.Product;
+
+@Database(entities = Product.class, exportSchema = false, version = 1)
+public abstract class ProductDatabase extends RoomDatabase {
+    private static final String DB_NAME = "products_db";
+    private static ProductDatabase instance;
+
+    public static synchronized ProductDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), ProductDatabase.class, DB_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
+
+    public abstract ProductDao productDao();
+}
--- app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/models/Product.java
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/models/Product.java
@@ -1,17 +1,22 @@
 package com.nirmalbhetwal.lab2_nirmal_c0841296_android.models;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import org.jetbrains.annotations.NotNull;
 
 @Entity(tableName = "products")
 public class Product {
     @PrimaryKey(autoGenerate = true)
     private int id;
+    @NotNull
     @ColumnInfo(name = "name")
     private String name;
+    @NotNull
     @ColumnInfo(name = "description")
     private String description;
+    @NotNull
     @ColumnInfo(name = "price")
     private double price;
 
@@ -20,6 +25,38 @@ public class Product {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 }
