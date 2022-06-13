9856398dd94a3afae8a8623b03e83577e94338d1
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-13 19:48:42 -0400

    created custom adapter

--- app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/adapters/ProductAdapter.java
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/adapters/ProductAdapter.java
@@ -1,11 +1,14 @@
 package com.nirmalbhetwal.lab2_nirmal_c0841296_android.adapters;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nirmalbhetwal.lab2_nirmal_c0841296_android.R;
 import com.nirmalbhetwal.lab2_nirmal_c0841296_android.models.Product;
 
 import java.util.List;
@@ -18,6 +21,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         this.context = context;
         this.productList = productList;
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -34,6 +38,15 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         return 0;
     }
 
-    public class ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvProductName, tvProductDescription, tvProductPrice;
+
+        public ViewHolder (View itemView) {
+            super(itemView);
+
+            this.tvProductName = (TextView) itemView.findViewById(R.id.tvProductName);
+            this.tvProductDescription = (TextView) itemView.findViewById(R.id.tvProductDescription);
+            this.tvProductPrice = (TextView) itemView.findViewById(R.id.tvProductPrice);
+        }
     }
 }
