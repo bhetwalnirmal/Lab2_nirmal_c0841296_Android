684677e2af095a043c5bd5d3649e37148e7c6f72
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-13 20:53:19 -0400

    removed item from the view

--- app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/activities/DashboardAcitvity.java
+++ app/src/main/java/com/nirmalbhetwal/lab2_nirmal_c0841296_android/activities/DashboardAcitvity.java
@@ -8,11 +8,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.nirmalbhetwal.lab2_nirmal_c0841296_android.R;
 import com.nirmalbhetwal.lab2_nirmal_c0841296_android.abstracts.ProductDatabase;
 import com.nirmalbhetwal.lab2_nirmal_c0841296_android.adapters.ProductAdapter;
+import com.nirmalbhetwal.lab2_nirmal_c0841296_android.listeners.ProductListTouchListener;
 import com.nirmalbhetwal.lab2_nirmal_c0841296_android.models.Product;
 
 import java.util.ArrayList;
@@ -50,5 +52,36 @@ public class DashboardAcitvity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ProductListTouchListener touchListener = new ProductListTouchListener(this, productsRecyclerView);
+        touchListener.setClickable(new ProductListTouchListener.OnRowClickListener() {
+            @Override
+            public void onRowClicked(int position) {
+                Toast.makeText(getApplicationContext(), productList.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onIndependentViewClicked(int independentViewID, int position) {
+
+            }
+        })
+            .setSwipeOptionViews(R.id.delete_task,R.id.edit_task)
+            .setSwipeable(R.id.rowFG, R.id.rowBG, new ProductListTouchListener.OnSwipeOptionsClickListener() {
+                @Override
+                public void onSwipeOptionClicked(int viewID, int position) {
+                    switch (viewID){
+                        case R.id.delete_task:
+                            productList.remove(position);
+                            productAdapter.setProductList(productList);
+                            break;
+                        case R.id.edit_task:
+                            Toast.makeText(getApplicationContext(),"Edit Not Available",Toast.LENGTH_SHORT).show();
+                            break;
+
+                    }
+                }
+            });
+
+        productsRecyclerView.addOnItemTouchListener(touchListener);
     }
 }
